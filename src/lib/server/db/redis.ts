a80814ec3ed4102351face9be25386ee75c56d76
Tanish Shah <connecttanish@gmail.com>
2025-01-26 01:26:26 -0800

    refactored logic that reads connection url for redis

--- src/lib/server/db/redis.ts
+++ src/lib/server/db/redis.ts
@@ -1,11 +1,22 @@
 import { dev } from '$app/environment';
-import { DB_REDIS_PASSWORD } from '$env/static/private';
-import { createClient } from 'redis';
+import { DB_REDIS_PASSWORD, DB_REDIS_URL } from '$env/static/private';
+import {
+	createClient,
+	type RedisClientOptions,
+	type RedisFunctions,
+	type RedisModules,
+	type RedisScripts,
+} from 'redis';
 
 const redisClientSingleton = async () => {
-	const client = await createClient({
-		password: DB_REDIS_PASSWORD,
-	}).connect();
+	const connectionParams: RedisClientOptions<RedisModules, RedisFunctions, RedisScripts> = {};
+	if (DB_REDIS_URL.length > 0) {
+		connectionParams.url = DB_REDIS_URL;
+	} else {
+		connectionParams.password = DB_REDIS_PASSWORD;
+	}
+
+	const client = await createClient(connectionParams).connect();
 
 	return client;
 };
