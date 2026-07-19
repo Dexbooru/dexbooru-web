14d752ac8aba6844864b093f15f0422a5d9b0ae4
Tanish Shah <connecttanish@gmail.com>
2026-07-18 19:31:37 -0600

    fix template file path resolution issue for html files related to auth workflows

--- src/lib/server/constants/email.ts
+++ src/lib/server/constants/email.ts
@@ -1,24 +1,10 @@
-import fs from 'fs';
-import path from 'path';
+import accountRecoveryEmailTemplate from '../email-templates/forgot-password.html?raw';
+import oauthPasswordEmailTemplate from '../email-templates/oauth-password.html?raw';
+import emailVerificationTemplate from '../email-templates/verify-email.html?raw';
 
-export const ACCOUNT_RECOVERY_EMAIL_TEMPLATE = fs.readFileSync(
-	path.join(process.env.PWD ?? '', 'src/lib/server/email-templates/forgot-password.html'),
-	{
-		encoding: 'utf-8',
-	},
-);
-export const OAUTH_PASSWORD_EMAIL_TEMPLATE = fs.readFileSync(
-	path.join(process.env.PWD ?? '', 'src/lib/server/email-templates/oauth-password.html'),
-	{
-		encoding: 'utf-8',
-	},
-);
-export const EMAIL_VERIFICATION_TEMPLATE = fs.readFileSync(
-	path.join(process.env.PWD ?? '', 'src/lib/server/email-templates/verify-email.html'),
-	{
-		encoding: 'utf-8',
-	},
-);
+export const ACCOUNT_RECOVERY_EMAIL_TEMPLATE = accountRecoveryEmailTemplate;
+export const OAUTH_PASSWORD_EMAIL_TEMPLATE = oauthPasswordEmailTemplate;
+export const EMAIL_VERIFICATION_TEMPLATE = emailVerificationTemplate;
 
 export const ACCOUNT_RECOVERY_EMAIL_SUBJECT = 'Dexbooru Account Recovery';
 export const EMAIL_VERIFICATION_SUBJECT = 'Verify your Dexbooru email address';
