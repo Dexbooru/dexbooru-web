9c4781b51bf7f2a2d190e9307575432ed20fa82e
Tanish Shah <36543258+t-shah02@users.noreply.github.com>
2026-09-26 13:25:31 -0600

    upgrade project dependenices to latest patch (#31)

--- src/lib/server/controllers/request-schemas/posts.ts
+++ src/lib/server/controllers/request-schemas/posts.ts
@@ -135,7 +135,7 @@ const CreatePostSchema = {
 				},
 			),
 		uploadId: z.string().uuid().optional(),
-		ignoreDuplicates: BoolStrSchema.optional().default(false),
+		ignoreDuplicates: BoolStrSchema,
 	}),
 } satisfies TRequestSchema;
 
